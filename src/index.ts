4d2ca60664b538f038e333fc1697e31c602971f4
Fabiano Brito <fabiano.brito@wolt.com>
2020-11-18 21:15:59 +0200

    Create wishlist concept

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import queryString from 'query-string';
 
 axios.defaults.headers.get['x-psn-store-locale-override'] = 'en-FI';
 
+const wishlist = [
+  { id: 'EP1004-CUSA08519_00-REDEMPTION000002', hash: '8532da7eda369efdad054ca8f885394a2d0c22d03c5259a422ae2bb3b98c5c99' }
+];
+
 type GameResponse = {
   data: {
     productRetrieve: {
@@ -23,23 +27,26 @@ const baseUrl = 'https://web.np.playstation.com/api/graphql/v1/op';
 
 const storeBaseLink = 'https://store.playstation.com/en-fi/product';
 
-const variables = {
-  productId: 'EP1004-CUSA08519_00-REDEMPTION000002'
-};
-const extensions = {
-  persistedQuery: {
-    version: 1,
-    sha256Hash: '8532da7eda369efdad054ca8f885394a2d0c22d03c5259a422ae2bb3b98c5c99',
-  }
-} ;
-const queryParams = {
-  operationName: 'productRetrieveForCtasWithPrice',
-  variables: JSON.stringify(variables),
-  extensions: JSON.stringify(extensions),
-};
-const params = queryString.stringify(queryParams);
+const promises = wishlist.map(game => {
+  const variables = { productId: game.id };
+  const extensions = { persistedQuery: { version: 1, sha256Hash: game.hash } };
+  const queryParams = {
+    operationName: 'productRetrieveForCtasWithPrice',
+    variables: JSON.stringify(variables),
+    extensions: JSON.stringify(extensions),
+  };
+  const params = queryString.stringify(queryParams);
+  return axios.get<GameResponse>(`${baseUrl}?${params}`);
+});
 
 (async () => {
-  const response = await axios.get<GameResponse>(`${baseUrl}?${params}`);
-  console.log(response.data.data.productRetrieve.webctas[0]);
+  const responses = await Promise.all(promises);
+  const output = responses.map(res => ({
+    name: res.data.data.productRetrieve.name,
+    normalPrice: res.data.data.productRetrieve.webctas[0].price.basePrice,
+    discountPrice: res.data.data.productRetrieve.webctas[0].price.discountedPrice,
+    discount: res.data.data.productRetrieve.webctas[0].price.discountText,
+    endTime: res.data.data.productRetrieve.webctas[0].price.endTime,
+  }))
+  console.log(output);
 })();
